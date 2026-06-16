3713f3fa1653b40255f8cd9770f97f78a27ab8a6
Gus <58223632+thegu5@users.noreply.github.com>
2026-06-15 20:51:42 -0700

    chore: remove no-longer-used code

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,6 @@
 import type { InferOutput } from "@atcute/lexicons";
 import type { Did } from "@atcute/lexicons/syntax";
 import type { SiteStandardPublication } from "@atcute/standard-site";
-import type { StandardSchemaV1 } from "@standard-schema/spec";
-import type { Loader } from "astro/loaders";
 
 export type PublicationConfig = {
 	collectionName: string;
@@ -13,10 +11,4 @@ export type PublicationConfig = {
 export type ScuteConfig = {
 	identity: Did;
 	publications: PublicationConfig[];
-};
-
-export type CollectionConfig = {
-	type: "content" | "data" | "content_layer";
-	schema: StandardSchemaV1 | ((context: object) => StandardSchemaV1);
-	loader: Loader;
 };
