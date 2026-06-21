4f681a8cee61e9981fc5f1587e640ba41857aa0c
Gus <58223632+thegu5@users.noreply.github.com>
2026-06-21 15:26:08 -0400

    fix(publish): make content collection schema compatible with @astrojs/rss

--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,11 @@
 import { z } from "astro/zod";
 
+// https://github.com/withastro/astro/blob/main/packages/astro-rss/src/schema.ts#L6
+const dateSchema = z
+	.union([z.string(), z.number(), z.date()])
+	.transform((value) => new Date(value))
+	.refine((value) => !Number.isNaN(value.getTime()))
+
 /**
  * Base schema for frontmatter properties relevant to scute
  */
@@ -8,8 +14,8 @@ export const scuteSchema = z
 		title: z.string(),
 		description: z.string().optional(),
 
-		pubDate: z.coerce.date().optional(),
-		publishedAt: z.coerce.date().optional(),
+		pubDate: dateSchema.optional(),
+		publishedAt: dateSchema.optional(),
 
 		categories: z.array(z.string()).optional(),
 		tags: z.array(z.string()).optional(),
