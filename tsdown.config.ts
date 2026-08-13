2fc18fe831f50c3d9a0ba487a31e5765aa0198f4
Gus <58223632+thegu5@users.noreply.github.com>
2026-08-13 13:14:49 -0400

    build: use tsc instead of tsdown

--- tsdown.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsdown";
-
-export default defineConfig({
-	fixedExtension: false,
-	entry: ["src/index.ts", "src/cli.ts", "src/middleware.ts"],
-	deps: {
-		neverBundle: [/^astro:/],
-	},
-});
